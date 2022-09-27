3d0de93f7c90feca97a77c8bd14145f56d6a463e
Luciano Ropero <lropero@gmail.com>
2022-09-27 08:57:38 -0300

    minor

--- classes/network.js
+++ classes/network.js
@@ -1,39 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-class NeuralNetwork {
-  constructor (layers) {
-    this.layers = []
-    for (let i = 0; i < layers.length - 1; i++) {
-      this.layers.push(new Layer({ inputCount: layers[i], outputCount: layers[i + 1] }))
-    }
-  }
-
-  static feedForward ({ inputs, network }) {
-    // eslint-disable-next-line no-undef
-    let outputs = Layer.feedForward({ activate: relu, inputs, layer: network.layers[0] })
-    for (let i = 1; i < network.layers.length; i++) {
-      // eslint-disable-next-line no-undef
-      outputs = Layer.feedForward({ activate: i === network.layers.length - 1 ? x => x : relu, inputs: outputs, layer: network.layers[i] })
-    }
-    // eslint-disable-next-line no-undef
-    return softmax(outputs)
-  }
-
-  static mutate ({ amount, network }) {
-    for (const layer of network.layers) {
-      for (let i = 0; i < layer.biases.length; i++) {
-        // eslint-disable-next-line no-undef
-        layer.biases[i] = lerp(layer.biases[i], Math.random(), amount)
-      }
-      for (let i = 0; i < layer.weights.length; i++) {
-        for (let j = 0; j < layer.weights[i].length; j++) {
-          // eslint-disable-next-line no-undef
-          layer.weights[i][j] = lerp(layer.weights[i][j], Math.random(), amount)
-        }
-      }
-    }
-  }
-}
-
 class Layer {
   constructor ({ inputCount, outputCount }) {
     this.biases = new Array(outputCount)
@@ -67,6 +31,42 @@ class Layer {
     for (let i = 0; i < layer.inputs.length; i++) {
       for (let j = 0; j < layer.outputs.length; j++) {
         layer.weights[i][j] = Math.random()
+      }
+    }
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+class NeuralNetwork {
+  constructor (layers) {
+    this.layers = []
+    for (let i = 0; i < layers.length - 1; i++) {
+      this.layers.push(new Layer({ inputCount: layers[i], outputCount: layers[i + 1] }))
+    }
+  }
+
+  static feedForward ({ inputs, network }) {
+    // eslint-disable-next-line no-undef
+    let outputs = Layer.feedForward({ activate: relu, inputs, layer: network.layers[0] })
+    for (let i = 1; i < network.layers.length; i++) {
+      // eslint-disable-next-line no-undef
+      outputs = Layer.feedForward({ activate: i === network.layers.length - 1 ? x => x : relu, inputs: outputs, layer: network.layers[i] })
+    }
+    // eslint-disable-next-line no-undef
+    return softmax(outputs)
+  }
+
+  static mutate ({ amount, network }) {
+    for (const layer of network.layers) {
+      for (let i = 0; i < layer.biases.length; i++) {
+        // eslint-disable-next-line no-undef
+        layer.biases[i] = lerp(layer.biases[i], Math.random(), amount)
+      }
+      for (let i = 0; i < layer.weights.length; i++) {
+        for (let j = 0; j < layer.weights[i].length; j++) {
+          // eslint-disable-next-line no-undef
+          layer.weights[i][j] = lerp(layer.weights[i][j], Math.random(), amount)
+        }
       }
     }
   }
